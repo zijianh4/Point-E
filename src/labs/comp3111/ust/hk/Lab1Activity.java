f9c409b4a9b7f1cdf78097abaffbee7a3ac7d53c
huangzijian <huangzijian@10.89.228.192>
2018-02-08 16:16:39 +0800

    Implemented the sum of 10 integers

--- src/labs/comp3111/ust/hk/Lab1Activity.java
+++ src/labs/comp3111/ust/hk/Lab1Activity.java
@@ -1,5 +1,17 @@
 package labs.comp3111.ust.hk;
 
 public class Lab1Activity {
-
+	
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int sum = 0;
+		
+		//Write your code here..
+		for(int i = 0; i < 10; i++) {
+			sum = sum + arr[i];
+		}
+		
+		System.out.println(String.format("Sum of the number is %d", sum));
+	}
 }
