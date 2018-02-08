2567ef2a067c3221d684401366144ff5410d1db2
huangzijian <huangzijian@10.89.228.192>
2018-02-08 16:01:52 +0800

    Added Lab1Activity.java

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -4,5 +4,6 @@ public class Lab1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Lab1: Introduction Git and Github");
+		System.out.println("Congratulations! You learned the Git basics");
 	}
 }
